2b218ecde30199abe7fa567308c531a454c489db
pipeline <pipeline@jmaconsulting.biz>
2023-11-08 14:02:45 -0700

    Civix Upgrade to 23.11.8

--- createuser.php
+++ createuser.php
@@ -13,15 +13,6 @@ function createuser_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function createuser_civicrm_xmlMenu(&$files) {
-  _createuser_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -31,73 +22,12 @@ function createuser_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function createuser_civicrm_uninstall() {
-  _createuser_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function createuser_civicrm_enable() {
   _createuser_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function createuser_civicrm_disable() {
-  _createuser_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function createuser_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _createuser_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function createuser_civicrm_managed(&$entities) {
-  _createuser_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function createuser_civicrm_caseTypes(&$caseTypes) {
-  _createuser_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function createuser_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _createuser_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 function createuser_civicrm_searchTasks($objectName, &$tasks) {
